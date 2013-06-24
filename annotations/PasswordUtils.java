763fdb0b21cd7d4f8356a4bacab83ffb59f9b757
Simon Xu <xixu@paypal.com>
2013-06-24 16:06:19 +0800

    first commit

---
+++ annotations/PasswordUtils.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class PasswordUtils {
+	@UseCase(id = 47, description = "Passwords must contain at least one numeric")
+	public boolean validatePassword(String password) {
+		return password.matches("\\w*\\d\\w*");
+	}
+
+	@UseCase(id = 49, description = "New passwords can't equal previously used ones")
+	public boolean checkForNewPassword(
+		List<String> prevPasswords, String password) {
+		return !prevPasswords.contains(password);
+	}	
+}
